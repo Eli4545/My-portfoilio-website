403d32f6689e512a1a3a63e7c213c8a625d42705
Eli4545 <elisimon4545@gmail.com>
2024-08-19 00:07:54 -0400

    created Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
+import eli from '../assets/eli.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] mx:auto py-8 bg-black'>
+
+        <div className='col-span-1 my-auto mx-auto w-[300px] h-auto lg:w-[400px]'>
+            <img src={eliimage} alt="hero image"/>
+        </div>
+
+        <div className='col-span-2 px-5 my-auto'>
+            <h1 className='text-white text-4xl sm:text-8xl font-extrabold'>
+                <span className='primary-color'>
+                    I am a
+                </span> <br/>
+                <TypeAnimation
+                sequence={[
+                    "Backend Dev",
+                    1000,
+
+                ]}
+                wrapper='span'
+                speed={50}
+                repeat={Infinity}
+                />
+            </h1>
+
+
+        </div>
+
+    </div>
   )
 }
 
